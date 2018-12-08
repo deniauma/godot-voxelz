8d77bf699ab966d053f6c01d84bdf6b1bb1caad2
Maryvonne Lemeunier <you@example.com>
2018-12-08 15:50:51 +0100

    Deferred chunk rendering working

--- world.gd
+++ world.gd
@@ -1,13 +1,13 @@
 extends Spatial
 
-var MAP_SIZE = 256
+var MAP_SIZE = 1024
 var CHUNK_SIZE = 16
 
 var chunk_class = load("res://scripts/chunk.gd")
 onready var player = $Player
 var map
 var highest
-var chunk_lib
+var chunk_lib = {}
 var thread = Thread.new()
 var curr_chuck
 var prev_chunk
@@ -18,10 +18,13 @@ func _ready():
 	var r = Utils.generate_heightmap(MAP_SIZE)
 	map = r[0]
 	highest = r[1]
-	chunk_lib = Utils.new_array2D(MAP_SIZE / CHUNK_SIZE, MAP_SIZE / CHUNK_SIZE)
+	#chunk_lib = Utils.new_array2D(MAP_SIZE / CHUNK_SIZE, MAP_SIZE / CHUNK_SIZE)
 	for x in range(5):
 		for y in range(5):
-			chunk_lib.set(x, y, create_chunk(Vector2(x,y)))
+			var c = create_chunk(Vector2(x,y))
+			add_child(c)
+			#chunk_lib.set(x, y, c)
+			chunk_lib[Vector2(x,y)] = c
 	player.transform.origin = Vector3(2*CHUNK_SIZE, 20, 2*CHUNK_SIZE)
 	
 func create_chunk(pos):
@@ -36,7 +39,6 @@ func create_chunk(pos):
 				chunk_map[Vector3(x - pos.x * CHUNK_SIZE, y, z - pos.y * CHUNK_SIZE)] = 1
 	var chunk = chunk_class.new()
 	chunk.translate(Vector3(pos.x * CHUNK_SIZE, 0, pos.y * CHUNK_SIZE))
-	add_child(chunk)
 	chunk.create(chunk_map, max_h)
 	#player.transform.origin.y = 200
 	print("Chunk added in "+str(OS.get_ticks_msec() - t)+" ms")
@@ -65,21 +67,39 @@ func load_chunks():
 		y_dir = curr_chuck.y + (diff.y / abs(diff.y)) * 2
 		#create_chunk(Vector2(curr_chuck.x, y_dir))
 	
-	for i in range(-1,2,1):
+	var c_pos
+	for i in range(-2,3,1):
 		if(diff.x != 0):
-			#create_chunk(Vector2(x_dir, curr_chuck.y + i))
-			chunks_queue.append(Vector2(x_dir, curr_chuck.y + i))
+			c_pos = Vector2(x_dir, curr_chuck.y + i)
+			if not chunk_lib.has(c_pos):
+				chunks_queue.append(c_pos)
 		if(diff.y != 0):
-			#create_chunk(Vector2(curr_chuck.x, y_dir + i))
-			chunks_queue.append(Vector2(curr_chuck.x, y_dir + i))
+			c_pos = Vector2(curr_chuck.x + i, y_dir)
+			if not chunk_lib.has(c_pos):
+				chunks_queue.append(c_pos)
+
+
+func on_mesh_generated(pos):
+	var chunk = thread.wait_to_finish()
+	add_child(chunk)
+	chunk_lib[pos] = chunk
 	
+
+func defer_render_chunk(pos):
+	var c = create_chunk(pos)
+	call_deferred("on_mesh_generated", pos)
+	return c
+
+	
+func generate_new_chunks():
+	if(chunks_queue.size() > 0 and not thread.is_active()):
+		thread.start(self, "defer_render_chunk", chunks_queue.pop_front())
+
 func _physics_process(delta):
 	var pos = player.transform.origin
 	get_tree().call_group("hud", "update_player_pos", pos)
-	get_tree().call_group("hud", "update_nb_chunks", nb_chunks, chunks_queue.size())
+	get_tree().call_group("hud", "update_nb_chunks", chunk_lib.size(), chunks_queue.size())
 	find_current_chunk(pos)
 			
 func _process(delta):
-	print(str(thread.is_active()))
-	if(chunks_queue.size() > 0 and not thread.is_active()):
-		thread.start(self, "create_chunk", chunks_queue.pop_front())
+	generate_new_chunks()
